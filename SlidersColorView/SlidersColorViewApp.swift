a5347bb94682ea7ead9b69c0e0d489f3a0c15090
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2023-12-15 17:30:09 +0500

    Initial Commit

---
+++ SlidersColorView/SlidersColorViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersColorViewApp.swift
+//  SlidersColorView
+//
+//  Created by Лилия Андреева on 15.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlidersColorViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
